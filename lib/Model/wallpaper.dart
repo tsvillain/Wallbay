60e563f012bf7a1382cd3d2fb03f6fbacd0f468c
Tekeshwar Singh <tekeshwarsingh2000@gmail.com>
2020-05-25 18:45:57 +0530

    redefined model

--- lib/Model/wallpaper.dart
+++ lib/Model/wallpaper.dart
@@ -1,27 +1,19 @@
 class Wallpaper {
-  String photographerName;
-  String photographerUrl;
   String original;
   String portrait;
 
   Wallpaper({
-    this.photographerName,
-    this.photographerUrl,
     this.original,
     this.portrait,
   });
 
   Wallpaper.fromMap(Map<String, dynamic> map) {
-    this.photographerName = map["photographer"];
-    this.photographerUrl = map["photographer_url"];
     this.original = map["src"]["original"];
     this.portrait = map["src"]["portrait"];
   }
 
   toJson() {
     return {
-      "photographer": this.photographerName,
-      "photographer_url": this.photographerUrl,
       "original": this.original,
       "portrait": this.portrait,
     };
