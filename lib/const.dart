6c56562417fcb3d2c8a656426461ed2dc34e393b
Tekeshwar Singh <tekeshwarsingh2000@gmail.com>
2021-01-27 21:07:35 +0530

    readme updated

--- lib/const.dart
+++ lib/const.dart
@@ -1,6 +1,5 @@
 ///Get API key from https://www.pexels.com/api/
-final String apiKey =
-    "563492ad6f9170000100000140f9dc05fbcd4d43a2b63528687b0825";
+final String apiKey = "";
 final String editorChoiceEndPoint =
     "https://api.pexels.com/v1/search?query=wallpaper";
 final String perPageLimit = "&per_page=30";
