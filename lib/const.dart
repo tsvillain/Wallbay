51624eb29e53810b83d18f9d40ba5ffabcbd8467
Tekeshwar Singh <tekeshwarsingh2000@gmail.com>
2021-01-27 21:05:51 +0530

    Android 10 Bug Fixed

--- lib/const.dart
+++ lib/const.dart
@@ -1,6 +1,6 @@
 ///Get API key from https://www.pexels.com/api/
 final String apiKey =
-    "";
+    "563492ad6f9170000100000140f9dc05fbcd4d43a2b63528687b0825";
 final String editorChoiceEndPoint =
     "https://api.pexels.com/v1/search?query=wallpaper";
 final String perPageLimit = "&per_page=30";
